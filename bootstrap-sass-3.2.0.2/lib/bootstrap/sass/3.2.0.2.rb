a2222e137bec7b6c2444561c1418d4dafa062846
Tim Chang <wtchang051592@gmail.com>
2014-10-07 00:50:44 -0400

    finish layout and routes

---
+++ bootstrap-sass-3.2.0.2/lib/bootstrap/sass/3.2.0.2.rb
@@ -0,0 +1,9 @@
+require "bootstrap/sass/3.2.0.2/version"
+
+module Bootstrap
+  module Sass
+    module 3.2.0.2
+      # Your code goes here...
+    end
+  end
+end
---
+++ bootstrap-sass-3.2.0.2/lib/bootstrap/sass/3.2.0.2/version.rb
@@ -0,0 +1,7 @@
+module Bootstrap
+  module Sass
+    module 3.2.0.2
+      VERSION = "0.0.1"
+    end
+  end
+end
